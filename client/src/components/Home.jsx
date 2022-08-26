39116e91d47d039e6d9dd5fc92670b92acbdaa58
Eleazar Udo <elzoremmanuel@gmail.com>
2022-08-26 23:34:32 +0300

    added background image to div

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,6 @@ export default function Home (){
 
 
     return <div className="home">
-        <button>Search for a game and join</button>
+        <button className="search-button">Search for a game and join</button>
     </div>
 }
