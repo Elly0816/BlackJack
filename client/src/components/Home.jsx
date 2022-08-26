5fc4e6b4b3a6d241898f17013950884d3e81ef97
Eleazar Udo <elzoremmanuel@gmail.com>
2022-08-27 00:21:42 +0300

    styled home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,10 @@ export default function Home (){
 
 
     return <div className="home">
-        <button className="search-button">Search for a game and join</button>
+            <div className="home-item">
+                <h2>WELCOME TO BLACKJACK</h2>
+                <button className="search-button">Search for a game and join</button>
+            </div>
+        
     </div>
 }
